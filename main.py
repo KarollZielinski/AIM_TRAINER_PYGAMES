1caaa8c1c2796e6732f8d2a1dafcb49346fec8bb
Karol <123296044+KarollZielinski@users.noreply.github.com>
2023-09-05 20:46:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import pygame
 pygame.init()
 
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 800, 600
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer")
